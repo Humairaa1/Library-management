fd2ad472001834e2a52e865725e82a192270254c
Humaira <humaira71bd@gmail.com>
2025-02-17 21:20:54 -0800

    librarian page completed

---
+++ js/shared.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function () {
+    fetch("navbar.html")
+      .then(response => response.text())
+      .then(data => {
+        document.getElementById("navbar-container").innerHTML = data;
+      });
+
+    fetch("footer.html")
+      .then(response => response.text())
+      .then(data => {
+        document.getElementById("footer-container").innerHTML = data;
+      });
+  });
+
+  
